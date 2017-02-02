a1d8721b007e4c6881657a6b07e3245e630f25fa
lovelifesp <lovelifesp@gmail.com>
2017-02-02 14:53:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var tempInF = prompt("temperature in f");
+var tempInC = (tempInF-32) * 5/9;
+
+console.log("the temperature is " + tempInC + "c ");
+
+
+var tempInF = (tempInF-32) * 5/9;
+var tempInC = prompt("temperature in c")
+
+console.log("the temperature is " + tempInF + "c ");
